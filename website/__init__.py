2f3b6feaefea4c474a15b8a57e41abd94409f6c6
River <river.swaris@outlook.com>
2025-08-10 11:31:51 +1200

    Tested and created sing up route

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 # Imported external libraries
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 
 db = SQLAlchemy()
@@ -29,6 +30,15 @@ def create_app():
     with app.app_context():
         db.create_all()
 
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth_login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+    
+    
 
 
     return app
