d92e93385c129108e9c8d0be439ba572508aa8ac
River <river.swaris@outlook.com>
2025-08-12 09:53:43 +1200

    Added comment function

--- website/views.py
+++ website/views.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, url_for, request, flash, redirect
+from flask_login import login_user, login_required, logout_user, current_user
+from .model import User, Note
+from . import db
 
 
 # Set blueprint
@@ -17,6 +20,17 @@ def home():
 def gallery():
     return render_template("gallery.html")
 
-@views.route("/contact")
+@views.route("/contact", methods=['POST', 'GET'])
+@login_required
 def contact():
+    if request.method == 'POST':
+        note = request.form.get('note')
+        if len(note) < 1:
+            flash('Comment cannot be blank!')
+        else: 
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Comment Added!', category='success')
+
     return render_template("contact.html")
